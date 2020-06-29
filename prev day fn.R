63b1a75a0b769e48d59bda68f5caff6c36a0f9f8
Isaac Silver <isaac.silver@temple.edu>
2020-06-29 12:20:06 -0500

    Forgot Saturday

--- prev day fn.R
+++ prev day fn.R
@@ -1,5 +1,5 @@
 prev_weekday = function(x = Sys.Date()){
-  if(weekdays(x) %in% c("Monday", "Sunday")){
+  if(weekdays(x) %in% c("Monday", "Saturday" ,"Sunday")){
     prev = "Friday"
   } else{prev = weekdays(x-1)}
   prev
